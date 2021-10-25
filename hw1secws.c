047d73688d05cd33dc0dcd41f41cb22912c33fb1
Itay1007 <69050067+Itay1007@users.noreply.github.com>
2021-10-25 11:40:43 +0300

    Add files via upload

---
+++ hw1secws.c
@@ -0,0 +1,76 @@
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO and for the 						Macros */
+
+#define PACKET_ACCEPT_MSG "*** Packet Accepted ***"
+#define PACKET_DROP_MSG  "*** Packet Dropped ***"
+/* create a global struct variable to use the hook of Netfilter*/
+static struct nf_hook_ops *nfho = NULL;
+
+
+static unsigned int hfunc(void *priv, struct sk_buff *skb,
+			  const struct nf_hook_state *state)
+{
+	struct iphdr *ip_header;
+	struct udphdr *udp_header;
+	
+	ipheader = ip_hdr(skb);
+	if()
+
+	/* if server to client directly drop the packet */
+	if(skb->dest == ip_clinet && skb->src == ip_server ||
+           skb->src == ip_server && skb_dest == ip_server)
+	{
+		printk(PACKET_DROP_MSG);
+		return NF_DROP;
+	}
+	printk(PACKET_ACCEPT_MSG);
+	return NF_ACCEPT;
+}
+
+
+
+
+
+
+
+/*
+ * this initialization function is called first  
+ * when we insert the module to the kernel
+ */
+static int __init my_module_init_function(void) {
+	/* print temporarly debug message to the kernel */
+	printk(KERN_INFO "Hello World!\n");
+	/* set the global struct pointer */
+	nfho = (struct nf_hool_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+
+	/* set the nfho fields */
+	nfho->hook = (nf_hookfn*) hfunc; /* set the hook function */ 
+	nfho->hooknum= NF_INET_PRE_ROUTING; /* verdict packet in the prerouting state*/
+	nfho->pf = PF_INET; /* for IPv4 */
+
+	nf_register_net_hook(&init_net, nfho);
+	return 0; /* if non-0 return means init_module failed */
+}
+ 
+/*
+ * this function is called last when 
+ * we remove the module from the kernel
+ */
+static void __exit my_module_exit_function(void) {
+
+	/* print to the kernel temporary debug message */
+	printk(KERN_INFO "Goodbye World!\n");
+	nf_unregister_net_hook(&init_net, nfho);
+	kfree(nfho);
+}
+
+module_init(my_module_init_function);
+module_exit(my_module_exit_function);
+
+
+
+
