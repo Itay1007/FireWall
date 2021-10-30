7ea590a4455648c7e42a613ea2f8b725a3ae1950
Itay Barok <itay.barok@gmail.com>
2021-10-30 16:46:26 +0300

    added prints

--- user_space.py
+++ user_space.py
@@ -14,12 +14,11 @@ def main():
             show_packets_status()
 
     except TypeError as type_e:
-        print("Invalid type [{0}] not int".format(args[1]))
+        print(type_e)
     except ValueError as val_e:
-        print("Invalid value [{0}] not zero".format(args[1]))
+        print(val_e)
     except RuntimeError as e:
-        print("Too many arguments [{0}] including program name in cli not program name and optional cleanup argument".format(len(args)))
-
+        print(e)
 
 def check_valid_input(args):
     # no cleanup argument
@@ -28,19 +27,32 @@ def check_valid_input(args):
     elif len(args) == 2:
         cleanup_arg = int(args[1])
         if type(cleanup_arg) != int:
-            raise TypeError("Invalid type ({0}) not int".format(args[1]))
+            raise TypeError("Invalid type [{0}] not int".format(args[1]))
         elif cleanup_arg != 0:
-            raise ValueError("Invalid value ({0}) not zero".format(args[1]))
+            raise ValueError("Invalid value [{0}] not zero".format(args[1]))
     else:  # too many arguments on cli
-        raise RuntimeError("Too many arguments ({0}) in cli not program name and optional cleanup argument".format(len(args)))
+        raise RuntimeError("Too many arguments [{0}] in cli not program name and optional cleanup argument".format(len(args)))
 
 
 def cleanup_packets_status():
-    print("In cleanup_packet_status()")
+    with open("/dev/dev1") as fd:
+        lst_values = fd.read()
+        values = lst_values.split("\n")
+        accpet_number = values[0]
+        drop_number = values[1]
+        total = values[0] + values[1]
+
+        print_packets_status(accpet_number, drop_number, total)
 
 
 def show_packets_status():
     print("In show_packets_status()")
+
+
+def print_packets_status(accpet_number, drop_number, total):
+    print(F"Number of accepted packets: {accpet_number}")
+    print(F"Number of dropped packets: {drop_number}")
+    print(F"Total number of packets: {total}")
 
 
 if __name__ == "__main__":
