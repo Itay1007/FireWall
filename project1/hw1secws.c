1705998e61577bae6d5addec8da920b3f4a7f981
Itay Barok <itay.barok@gmail.com>
2021-10-30 17:19:23 +0300

    added file

--- project1/hw1secws.c
+++ project1/hw1secws.c
@@ -2,25 +2,33 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO and for the 						Macros */
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
-
+#include <linux/tcp.h>
+#include <linux/udp.h>
 
 #define PACKET_ACCEPT_MSG "*** Packet Accepted ***"
 #define PACKET_DROP_MSG  "*** Packet Dropped ***"
 
+MODULE_LICENSE("GPL");
 
 /* create a global struct variable to use the hook of Netfilter*/
 static struct nf_hook_ops *nfho = NULL;
 
-
-static unsigned int hfunc(void *priv, struct sk_buff *skb,
+/* packets between server and client need to  be drop */
+static unsigned int hfuncInForward(void *priv, struct sk_buff *skb,
 			  const struct nf_hook_state *state)
 {
-	if(state->hook == NF_INET_FORWARD)
-	{
-		printk(PACKET_DROP_MSG);
-		return NF_DROP;
-	}
-	
+	printk(PACKET_DROP_MSG);
+	return NF_DROP;
+}
+
+
+/* packets between server and FW or client and FW is acceped */
+static unsigned int hfuncInInput(void *priv, struct sk_buff *skb,
+			  const struct nf_hool_state *state)
+{
+	if(!skb)
+		return NF_ACCEPT;
+
 	printk(PACKET_ACCEPT_MSG);
 	return NF_ACCEPT;
 }
@@ -35,14 +43,15 @@ static int __init my_module_init_function(void) {
 	nfho = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 
 	/* set the nfho fields */
-	nfho->hook = (nf_hookfn*) hfunc; /* set the hook function */ 
+	nfho->hook = (nf_hookfn*) hfuncInForward; /* set the hook function */ 
 	nfho->hooknum = NF_INET_FORWARD; /* verdict packet in the prerouting state*/
 	nfho->pf = PF_INET; /* for IPv4 */
 
 	nf_register_net_hook(&init_net, nfho);
 
-	nfho->hooknum = NF_INET_LOCAL_IN;
 
+	nfho->hook = (nf_hookfn*) hfuncInInput;
+	nfho->hooknum = NF_INET_LOCAL_IN;
 	nf_register_net_hook(&init_net, nfho);
 
 	return 0; /* if non-0 return means init_module failed */
@@ -54,16 +63,11 @@ static int __init my_module_init_function(void) {
  */
 static void __exit my_module_exit_function(void) {
 
-	/* print to the kernel temporary debug message */
-	printk("In an exit");
 	nf_unregister_net_hook(&init_net, nfho);
 	kfree(nfho);
 }
 
 module_init(my_module_init_function);
 module_exit(my_module_exit_function);
-
-
-
 
 
