85a197e77991857f65cb497a84ae62a8be015f8c
Itay Barok <itay.barok@gmail.com>
2022-03-10 23:13:12 +0200

    commented out

--- project/src/filtering.c
+++ project/src/filtering.c
@@ -26,18 +26,19 @@ static int fw_free_ent_index = 0;
 
 ssize_t display(struct device *dev, struct device_attribute *attr, char *buf)	//sysfs show implementation
 {
-	return scnprintf(buf, PAGE_SIZE, "%s", str_of_table()); // set format and data in file
+	return scnprintf(buf, PAGE_SIZE, "%s", "hi");
+	//return scnprintf(buf, PAGE_SIZE, "%s", str_of_table()); // set format and data in file
 }
 
 char * str_of_table()
 {
 	char *str = "";
 
-	int i;
-	for(i = 0; i < fw_free_ent_index; i++)
-	{
-		sprintf(str, "%s%s", str, str_of_table_ent(i));
-	}
+	// int i;
+	// for(i = 0; i < fw_free_ent_index; i++)
+	// {
+	// 	sprintf(str, "%s%s", str, str_of_table_ent(i));
+	// }
 
 	return str;
 }
@@ -72,102 +73,102 @@ ssize_t modify(struct device *dev, struct device_attribute *attr, const char *bu
 	char * str_fw_table;
 	char * str_rule;
 	char * str_rule_field;
-	const char sep[3] = "\r\n"
-	const char fields_sep[2] = " "
-	sscanf(buf, "%s", &str_fw_table) // check for no errors with return value
+	const char sep[3] = "\r\n";
+	const char fields_sep[2] = " ";
+	sscanf(buf, "%s", &str_fw_table);
 	
 	str_rule = strtok(str_fw_table, sep);
 
-	while(str_rule != NULL)
-	{
-		str_rule_field = strtok(str_rule, fields_sep);
+// 	while(str_rule != NULL)
+// 	{
+// 		str_rule_field = strtok(str_rule, fields_sep);
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].rule_name = str_rule_field;
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].rule_name = str_rule_field;
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].direction = str_rule_field;		
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].direction = str_rule_field;		
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].src_ip = str_rule_field;
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].src_ip = str_rule_field;
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].src_prefix_size = str_rule_field;		
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].src_prefix_size = str_rule_field;		
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].dst_ip = str_rule_field;
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].dst_ip = str_rule_field;
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].dst_prefix_size = str_rule_field;		
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].dst_prefix_size = str_rule_field;		
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].protocol = str_rule_field;
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].protocol = str_rule_field;
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].src_port = str_rule_field;		
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].src_port = str_rule_field;		
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].dst_port = str_rule_field;		
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].dst_port = str_rule_field;		
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].ack = str_rule_field;		
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].ack = str_rule_field;		
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		if(str_rule_field != NULL)
-		{
-			fw_table[fw_free_ent_index].action = str_rule_field;		
-			str_rule_field = strtok(str_rule, NULL);
-		}
+// 		if(str_rule_field != NULL)
+// 		{
+// 			fw_table[fw_free_ent_index].action = str_rule_field;		
+// 			str_rule_field = strtok(str_rule, NULL);
+// 		}
 
-		fw_free_ent_index++;
-		str_rule = strtok(str_fw_table, NULL);
-	}
+// 		fw_free_ent_index++;
+// 		str_rule = strtok(str_fw_table, NULL);
+// 	}
 
-	return count;	
-}
+// 	return count;	
+// }
 
 
 int verdict_packet(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	int i;
 
-	for(i = 0; i < fw_free_ent_index; i++)
-	{
-		if(verdict_paket_with_rule(priv, skb, state, fw_table[i]) == ACCEPT)
-		{
-			return ACCEPT;
-		}
-	}
+	// for(i = 0; i < fw_free_ent_index; i++)
+	// {
+	// 	if(verdict_paket_with_rule(priv, skb, state, fw_table[i]) == ACCEPT)
+	// 	{
+	// 		return ACCEPT;
+	// 	}
+	// }
 
 	return DROP;
 }
