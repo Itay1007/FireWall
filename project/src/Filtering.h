d665161c3aa7706c9240889ebbba02642a55925a
Itay Barok <itay.barok@gmail.com>
2022-03-11 16:11:39 +0200

    moved things to one and main kernel module

---
+++ project/src/Filtering.h
@@ -0,0 +1,4 @@
+char * str_of_table(void);
+char * str_of_table_ent(int i);
+int verdict_packet(void *priv, struct sk_buff *skb, const struct nf_hook_state *state);
+int verdict_paket_with_rule(void *priv, struct sk_buff *skb, const struct nf_hook_state *state, rule_t rule)
--- project/src/hw3secws.c
+++ project/src/hw3secws.c
@@ -13,8 +13,22 @@ MODULE_AUTHOR("Itay Barok");
 /* create a global struct variable to use the hook of Netfilter*/
 static struct nf_hook_ops *nfho = NULL;
 
+
+static int major_number;
+static struct class* sysfs_class = NULL;
+static struct device* sysfs_device = NULL;
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE
+};
+
+
+
 // static unsigned int packets_accept_number = 0;
 // static unsigned int packets_drop_number = 0;
+static DEVICE_ATTR(sysfs_att, S_IWUSR | S_IRUGO , display, modify);
+
+
 
 
 static unsigned int hfuncInForward(void *priv, struct sk_buff *skb,
@@ -51,8 +65,37 @@ static int __init my_module_init_function(void) {
 		return -1;
 	}
 
-	my_filtering_table_init_function();
-	//my_logs_table_init_function();
+		//create char device
+	major_number = register_chrdev(0, "Sysfs_Device", &fops);
+	if (major_number < 0)
+		return -1;
+		
+	//create sysfs class
+	sysfs_class = class_create(THIS_MODULE, "fw");
+	if (IS_ERR(sysfs_class))
+	{
+		unregister_chrdev(major_number, "Sysfs_Device");
+		return -1;
+	}
+	
+	//create sysfs device
+	sysfs_device = device_create(sysfs_class, NULL, MKDEV(major_number, 0), NULL, "rules");	
+	if (IS_ERR(sysfs_device))
+	{
+		class_destroy(sysfs_class);
+		unregister_chrdev(major_number, "Sysfs_Device");
+		return -1;
+	}
+	
+	//create sysfs file attributes	
+	if (device_create_file(sysfs_device, (const struct device_attribute *)&dev_attr_sysfs_att.attr))
+	{
+		device_destroy(sysfs_class, MKDEV(major_number, 0));
+		class_destroy(sysfs_class);
+		unregister_chrdev(major_number, "Sysfs_Device");
+		return -1;
+	}
+
 
 	return 0; /* if non-0 return means init_module failed */
 }
@@ -66,6 +109,12 @@ static void __exit my_module_exit_function(void) {
 
 	nf_unregister_net_hook(&init_net, nfho);
 	kfree(nfho);
+
+	device_remove_file(sysfs_device, (const struct device_attribute *)&dev_attr_sysfs_att.attr);
+	device_destroy(sysfs_class, MKDEV(major_number, 0));
+	class_destroy(sysfs_class);
+	unregister_chrdev(major_number, "Sysfs_Device");
+
 
 	my_filtering_table_exit_function();
 	//my_logs_table_exit_function();
