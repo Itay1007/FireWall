9a977856b68363e12fb566dd6d991e71e87cb2fe
Itay Barok <itay.barok@gmail.com>
2022-04-01 20:19:42 +0300

    sync

---
+++ project/aliases.bash
@@ -0,0 +1,8 @@
+alias clean='make clean'
+alias pull='git pull'
+alias load='insmod hw3secws.ko'
+alias unload='rmmod hw3secws'
+alias reset='rm -rf /root/FireWall; git clone https://github.com/Itay1007/FireWall'
+alias class='cd /sys/class'
+alias rules='cd /sys/class/fw/rules'
+alias log='cd /dev/log'
