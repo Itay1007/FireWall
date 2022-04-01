9d50fa2877e03d6862ab88b7772b86d96325cd52
Itay Barok <itay.barok@gmail.com>
2022-04-01 10:52:33 +0300

    sync

--- project/char/char.c
+++ project/char/char.c
@@ -1,60 +1,54 @@
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 
-#include "fw.h"
+static dev_t first; // Global variable for the first device number
+static struct cdev c_dev; // Global variable for the character device structure
+static struct class *cl; // Global variable for the device class
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Itay Barok");
-
-
-struct cdev c_dev;
-// static struct device *dev;
-
-
-//---- Itay Barok Custom file operations of the log char device ----
-
-
-int my_open(struct inode *_inode, struct file *_file)
+static int my_open(struct inode *i, struct file *f)
 {
-	printk(KERN_INFO "Driver: open()\n");
-	return 0;
+    printk(KERN_INFO "Driver: open()\n");
+    return 0;
+}
+static int my_close(struct inode *i, struct file *f)
+{
+    printk(KERN_INFO "Driver: close()\n");
+    return 0;
+}
+static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+{
+    printk(KERN_INFO "Driver: read()\n");
+    return 0;
+}
+static ssize_t my_write(struct file *f, const char __user *buf, size_t len,
+    loff_t *off)
+{
+    printk(KERN_INFO "Driver: write()\n");
+    return len;
 }
 
-int my_close(struct inode *_inode, struct file *_file)
+static struct file_operations pugs_fops =
 {
-	printk(KERN_INFO "Driver: close()\n");
-	return 0;
-}
-
-ssize_t my_read(struct file *flip, char* buff, size_t length, loff_t *offp)
-{
-	printk(KERN_INFO "Driver: read()\n");
-	return 0;
-}
-
-ssize_t my_write(struct file *flip, const char* buff, size_t length, loff_t *offp)
-{
-	printk(KERN_INFO "Driver: write()\n");
-	return length;
-}
-
-static struct file_operations fops = {
-	.owner = THIS_MODULE,
-	.open = my_open,
-	.release = my_close,
-	.read = my_read,
+    .owner = THIS_MODULE,
+    .open = my_open,
+    .release = my_close,
+    .read = my_read,
     .write = my_write
 };
 
-/*
- * this initialization function is called first  
- * when we insert the module to the kernel
- */
-static int __init my_module_init_function(void) {
+static int __init ofcd_init(void) /* Constructor */
+{
+    int ret;
+    struct device *dev_ret;
 
-	int ret;
-	struct device *dev_ret;
-
-
-	 if ((ret = alloc_chrdev_region(&first, 0, 1, "Shweta")) < 0)
+    printk(KERN_INFO "Namaskar: ofcd registered");
+    if ((ret = alloc_chrdev_region(&first, 0, 1, "Shweta")) < 0)
     {
         return ret;
     }
@@ -78,23 +72,21 @@ static int __init my_module_init_function(void) {
         unregister_chrdev_region(first, 1);
         return ret;
     }
-
-	return 0; /* if non 0 return means init_module failed */
+    return 0;
 }
- 
 
-/*
- * this function is called last when 
- * we remove the module from the kernel
- */
-static void __exit my_module_exit_function(void) {
-	cdev_del(&c_dev);
+static void __exit ofcd_exit(void) /* Destructor */
+{
+    cdev_del(&c_dev);
     device_destroy(cl, first);
     class_destroy(cl);
     unregister_chrdev_region(first, 1);
+    printk(KERN_INFO "Alvida: ofcd unregistered");
 }
 
-// call this init function when loading this kernel module 
-module_init(my_module_init_function);
-// call this exit function when unloading this kernel module 
-module_exit(my_module_exit_function);
+module_init(ofcd_init);
+module_exit(ofcd_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Anil Kumar Pugalia <email@sarika-pugs.com>");
+MODULE_DESCRIPTION("Our First Character Driver");
