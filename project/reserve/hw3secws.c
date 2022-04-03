e4b77bc16e994cd2b94f571766dd76b84d456d74
Itay Barok <itay.barok@gmail.com>
2022-04-03 09:04:19 +0300

    sync

--- project/reserve/hw3secws.c
+++ project/reserve/hw3secws.c
@@ -29,32 +29,32 @@ static struct class *cl; // Global variable for the device class
 // 	.owner = THIS_MODULE
 // };
 
-ssize_t display(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	printk(KERN_INFO "display()\n");
-	return 0;
-}
+// ssize_t display(struct device *dev, struct device_attribute *attr, char *buf)
+// {
+// 	printk(KERN_INFO "display()\n");
+// 	return 0;
+// }
 
-ssize_t modify(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
-{	
-	printk(KERN_INFO "modify()\n");
-	return 0;
-}
+// ssize_t modify(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+// {	
+// 	printk(KERN_INFO "modify()\n");
+// 	return 0;
+// }
 
-ssize_t another_display(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	printk(KERN_INFO "another_display()\n");
-	return 0;
-}
+// ssize_t another_display(struct device *dev, struct device_attribute *attr, char *buf)
+// {
+// 	printk(KERN_INFO "another_display()\n");
+// 	return 0;
+// }
 
-ssize_t another_modify(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
-{	
-	printk(KERN_INFO "another_modify()\n");
-	return 0;
-}
+// ssize_t another_modify(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+// {	
+// 	printk(KERN_INFO "another_modify()\n");
+// 	return 0;
+// }
 
-static DEVICE_ATTR(my_life_my_rules, S_IWUSR | S_IRUGO , display, modify);
-static DEVICE_ATTR(my_world_inside, S_IWUSR | S_IRUGO , another_display, another_modify);
+// static DEVICE_ATTR(my_life_my_rules, S_IWUSR | S_IRUGO , display, modify);
+// static DEVICE_ATTR(my_world_inside, S_IWUSR | S_IRUGO , another_display, another_modify);
 
 
 
@@ -104,56 +104,56 @@ static int __init my_module_init_function(void) {
 
 	printk(KERN_INFO "Itay Barok's init()");
 
-	/* set the global struct pointer */
-	nfho = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+	// /* set the global struct pointer */
+	// nfho = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 
-	/* set the nfho fields */
-	nfho->hook = (nf_hookfn*) hfuncInForward; /* set the hook function */ 
-	nfho->hooknum = NF_INET_FORWARD; /* verdict packet in the prerouting state*/
-	nfho->pf = PF_INET; /* for IPv4 */
+	// /* set the nfho fields */
+	// nfho->hook = (nf_hookfn*) hfuncInForward; /* set the hook function */ 
+	// nfho->hooknum = NF_INET_FORWARD; /* verdict packet in the prerouting state*/
+	// nfho->pf = PF_INET; /* for IPv4 */
 
-	if(nf_register_net_hook(&init_net, nfho))
-		return -1;
+	// if(nf_register_net_hook(&init_net, nfho))
+	// 	return -1;
 
-	//create char device
-	major_number = register_chrdev(0, "rules", &rules_fops);
-	if (major_number < 0)
-		return -1;
+	// //create char device
+	// major_number = register_chrdev(0, "rules", &rules_fops);
+	// if (major_number < 0)
+	// 	return -1;
 		
-	//create sysfs class
-	sysfs_class = class_create(THIS_MODULE, "fw");
-	if (IS_ERR(sysfs_class))
-	{
-		unregister_chrdev(major_number, "rules");
-		return -1;
-	}
+	// //create sysfs class
+	// sysfs_class = class_create(THIS_MODULE, "fw");
+	// if (IS_ERR(sysfs_class))
+	// {
+	// 	unregister_chrdev(major_number, "rules");
+	// 	return -1;
+	// }
 	
-	//create sysfs device
-	sysfs_device = device_create(sysfs_class, NULL, MKDEV(major_number, 0), NULL, "rules");	
-	if (IS_ERR(sysfs_device))
-	{
-		class_destroy(sysfs_class);
-		unregister_chrdev(major_number, "rules");
-		return -1;
-	}
+	// //create sysfs device
+	// sysfs_device = device_create(sysfs_class, NULL, MKDEV(major_number, 0), NULL, "rules");	
+	// if (IS_ERR(sysfs_device))
+	// {
+	// 	class_destroy(sysfs_class);
+	// 	unregister_chrdev(major_number, "rules");
+	// 	return -1;
+	// }
 
-	//create sysfs file attributes	
-	if (device_create_file(sysfs_device, (const struct device_attribute *)&dev_attr_my_life_my_rules.attr))
-	{
-		device_destroy(sysfs_class, MKDEV(major_number, 0));
-		class_destroy(sysfs_class);
-		unregister_chrdev(major_number, "rules");
-		return -1;
-	}
+	// //create sysfs file attributes	
+	// if (device_create_file(sysfs_device, (const struct device_attribute *)&dev_attr_my_life_my_rules.attr))
+	// {
+	// 	device_destroy(sysfs_class, MKDEV(major_number, 0));
+	// 	class_destroy(sysfs_class);
+	// 	unregister_chrdev(major_number, "rules");
+	// 	return -1;
+	// }
 
-	// create sysfs file attributes	
-	if (device_create_file(sysfs_device, (const struct device_attribute *)&dev_attr_my_world_inside.attr))
-	{
-		device_destroy(sysfs_class, MKDEV(major_number, 0));
-		class_destroy(sysfs_class);
-		unregister_chrdev(major_number, "rules");
-		return -1;
-	}
+	// // create sysfs file attributes	
+	// if (device_create_file(sysfs_device, (const struct device_attribute *)&dev_attr_my_world_inside.attr))
+	// {
+	// 	device_destroy(sysfs_class, MKDEV(major_number, 0));
+	// 	class_destroy(sysfs_class);
+	// 	unregister_chrdev(major_number, "rules");
+	// 	return -1;
+	// }
 
     if ((ret = alloc_chrdev_region(&first, 0, 1, "Shweta")) < 0)
     {
